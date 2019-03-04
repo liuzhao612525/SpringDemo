6f7c047a15ade671ab6eb8dbc9a4c05db7bb081c
liuzhao <liuzhao612525@163.com>
2019-03-04 18:36:34 +0800

    ddd

--- src/cn/itcast/property/Person.java
+++ src/cn/itcast/property/Person.java
@@ -1,5 +1,9 @@
 package cn.itcast.property;
 
+import java.awt.List;
+import java.util.Map;
+import java.util.Properties;
+
 public class Person {
 	private String pname;
 	
@@ -7,7 +11,31 @@ public class Person {
 		this.pname = pname;
 	}
 	public void test1() {
-		System.out.println("name..........." + pname);
+//		System.out.println("name..........." + pname);
+		System.out.println("arrs..........." + arrs);
+		System.out.println("list..........." + list);
+		System.out.println("map..........." + map);
+		System.out.println("properties..........." + properties);
 	}
+	
+	private String[] arrs;
+	private List list;
+	private Map<String,String> map;
+	private Properties properties;
+	
+	
+	public void setArrs(String[] arrs) {
+		this.arrs = arrs;
+	}
+	public void setList(List list) {
+		this.list = list;
+	}
+	public void setMap(Map<String, String> map) {
+		this.map = map;
+	}
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+	
 
 }
